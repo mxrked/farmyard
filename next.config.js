907ce4561077df19f42abb7c93ab10075748f915
Parker <pp101socials@gmail.com>
2023-07-10 08:43:44 -0400

    Added site url

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = {
 
   //! This added clean urls and eliminates the .html extension aswell as linking
   //! CSS and JS with the trailingSlash
-  assetPrefix: isProd ? "PUT_URL_HERE" : "",
+  assetPrefix: isProd ? "https://tranquil-starlight-1772c5.netlify.app/" : "",
   trailingSlash: true,
 
   // This adds the _headers file after npm run build
