fcb0e6ecc2646ef9fdca0c78f99422a707b7b8f1
Parker <pp101socials@gmail.com>
2023-07-11 11:03:50 -0400

    Added icon

--- assets/cdns/CDNIcons.js
+++ assets/cdns/CDNIcons.js
@@ -11,4 +11,7 @@ const FAVICON_32 = CDNIconReturn("tab-icons", "favicon-32x32", "png");
 const FAVICON_16 = CDNIconReturn("tab-icons", "favicon-16x16", "png");
 const ATI = CDNIconReturn("tab-icons", "apple-touch-icon", "png");
 
+const LOGO = CDNIconReturn("logo", "logo", "webp");
+
 export { FAVICON, FAVICON_16, FAVICON_32, ATI };
+export { LOGO };
