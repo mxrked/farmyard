f7ad17a1fdb8bbcbb4b3f6d4236603add0720ba1
Parker <pp101socials@gmail.com>
2023-07-11 11:48:29 -0400

    Added TriggerExitAnimations function

--- pages/404.js
+++ pages/404.js
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 // Library Imports
 
 // Data/Functions/Images Imports
+import { TriggerExitAnimations } from "@/assets/functions/dom/triggers/TriggerExitAnimations";
 
 // Component Imports
 import { PageHead } from "@/assets/components/global/Misc/PageHead";
@@ -16,6 +17,11 @@ import "../assets/styles/modules/404/404.module.css";
 
 export default function NotFound() {
   const router = useRouter();
+
+  // Triggering exit animations
+  useEffect(() => {
+    TriggerExitAnimations();
+  }, []);
 
   return (
     <div id="PAGE" className="overrides_404 page full-second">
